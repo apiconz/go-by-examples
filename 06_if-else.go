fe41ee1283f029f858857db06be96efd0376ae94
Armando-Satellite <apiconz@gmail.com>
2015-08-10 00:42:40 -0500

    Example 6 If Else

--- 06_if-else.go
+++ 06_if-else.go
@@ -14,6 +14,8 @@ func main() {
 		fmt.Println("8 es divisible por 4")
 	}
 
+	//se pueden agregar una sentencia antes del condicional
+	//dichas sentencia estará disponible para el resto
 	if num := 9; num < 0 {
 		fmt.Println(num, "es negativo")
 	} else if num < 10 {
@@ -21,4 +23,6 @@ func main() {
 	} else {
 		fmt.Println(num, "tiene multiples digitos")
 	}
+
+	//en Go no existen las expresiones ternarias
 }
